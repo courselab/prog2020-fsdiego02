de40612a99ba1c12293f7e54a7b7ae16e741e475
fsdiego02 <63001506+fsdiego02@users.noreply.github.com>
2020-07-07 14:35:38 -0300

    m009 entregue

--- m009.c
+++ m009.c
@@ -16,15 +16,68 @@
 
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 100
 
+int vec_len (int *vec)
+{
+  int k = -1;
+  while (*(vec++))
+    k++;
+  return k;
+}
+
 /* Sort the first 'n' integers values in 'vector'. */
 
-void sort (int* vector, int n)
+void sort (int* vector, int s)
 {
+  if (s < 1)
+  {
+    return;
+  }
+  int counter = 0;
+  int r = 0; 
+  int rr = 0;
+  int value = MAX;
+  int siz = vec_len (vector); 
+  int newvec [siz + 1];
+
+  if (siz != 1)
+  {
+    while(r != siz - 2)
+    {
+      if (value != MAX)
+      {
+        value = MAX;
+      }
+      for(rr = 0; rr <siz - 1; rr++)
+      {
+        if(value >= vector [rr])
+        {
+          value = vector [rr]; 
+        }
+      }
+      for(rr = 0; rr < siz - 1; rr++)
+      { 
+        if(value == vector [rr] && counter !=1)
+        {
+          vector[rr] = 101;
+          counter = 1;
+        }
+      } 
+      counter = 0; 
+      newvec [r] = value; 
+      r++;
+    }
+    for (r = 0; r <siz - 1; r++)
+    {
+      vector [r] = 0;
+      vector [r] = newvec [r];
+    }
+  }
 }
 
 #define USAGE "m009 <num1> <nun2> ... \n"
