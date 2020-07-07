b644551be8741e455296b36637fefb7651b36e35
fsdiego02 <63001506+fsdiego02@users.noreply.github.com>
2020-07-07 13:57:04 -0300

    m005 entregue

--- m005.c
+++ m005.c
@@ -27,7 +27,16 @@
 
 int determinant (int m[3][3])
 {
-  return 0;
+  int result = 0, det1 = 0, det2 = 0;
+  det1 += (m[0][0] * m[1][1] * m[2][2]);
+  det1 += (m[0][1] * m[1][2] * m[2][0]);
+  det1 += (m[0][2] * m[1][0] * m[2][1]);
+  det2 += (m[0][2] * m[1][1] * m[2][0]);
+  det2 += (m[0][0] * m[1][2] * m[2][1]);
+  det2 += (m[0][1] * m[1][0] * m[2][2]);
+  result = det1 - det2;
+
+  return result;
 }
 
 
